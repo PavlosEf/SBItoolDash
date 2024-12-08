a2da69564b7162bbbde9386e7b6e7d24e09ae868
PavlosEf <pefthimiadis1@gmail.com>
2024-12-08 15:20:33 +0200

    Update SBItoolDash.py

--- SBItoolDash.py
+++ SBItoolDash.py
@@ -101,7 +101,7 @@ if selected_tool == "Off Prices Calculator":
             display: flex; /* Use flexbox for alignment */
             align-items: center; /* Vertically align items */
             justify-content: flex-start; /* Align items to the left */
-            gap: 20px; /* Add spacing between the percentage and label boxes */
+            gap: 2px; /* Add spacing between the percentage and label boxes */
             margin-top: 5px; /* Optional: Add some space above the container */
         }
 
